749354ab0ea714944d5e0ff001e747b1dfa54ca0
severedsolo <severedsolo@users.noreply.github.com>
2020-04-19 14:41:54 +0100

    Initial Commit

---
+++ ModuleCommandLimitedControl/ModuleCommandLimitedControl.cs
@@ -0,0 +1,18 @@
+﻿using CommNet;
+
+namespace ModuleCommandLimitedControl
+{
+    public class ModuleCommandLimitedControl : ModuleCommand
+    {
+        public override VesselControlState UpdateControlSourceState()
+        {
+            VesselControlState baseState = base.UpdateControlSourceState();
+            if (baseState == VesselControlState.ProbeFull)
+            {
+                moduleState = ModuleControlState.PartialProbe;
+                return VesselControlState.ProbePartial;
+            }
+            return baseState;
+        }
+    }
+}
